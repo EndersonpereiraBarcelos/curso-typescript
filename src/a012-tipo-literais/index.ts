b54029c25dbad0011ff6359e7b4ff4d8ac91f092
endersonpb <souza98120@gmail.com>
2023-01-25 19:15:05 -0300

    literais

--- src/a012-tipo-literais/index.ts
+++ src/a012-tipo-literais/index.ts
@@ -1,1 +1,16 @@
 let x = 10
+x = 0b1010
+const y = 10
+const a = 100
+const pessoa = {
+    name:'Enderson' as const,
+    lastName:'Pererira',
+} 
+
+function escolhaCor(cor: 'Vermelho' |'Azul' | 'Amarelo'): string{
+    return cor
+}
+
+console.log(escolhaCor('Azul'))
+
+export default 1;
