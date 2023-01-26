cdae571559fbaddfefafe682f0760061bbb9028d
endersonpb <souza98120@gmail.com>
2023-01-26 13:25:52 -0300

    lias, intersection

---
+++ src/a013-type-alias/index.ts
@@ -0,0 +1,24 @@
+type Idade = number
+type Pessoa = { 
+    name:string
+    idade: Idade
+    salario: number
+    corPreferida?: string
+}
+
+type corRGB = 'Vermelho' | 'Azul ' | 'Amarelo'
+type corCMYK = 'Ciano' | 'Magenta' | 'Amarelo' | 'Preto'
+type CorPreferida = corRGB | corCMYK
+
+const pessoa: Pessoa = {
+    name: 'Enderson',
+    idade: 19,
+    salario: 3.200
+    
+}
+
+export function setCorPreferida(pessoa: Pessoa, cor: CorPreferida): Pessoa{
+    return{ ...pessoa, corPreferida: cor}
+}
+console.log(pessoa)
+console.log(setCorPreferida(pessoa, 'Ciano'))
